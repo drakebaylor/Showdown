06d44d03593a416ddc4c11b8a751498dd1f146be
drakebaylor <47865987+drakebaylor@users.noreply.github.com>
2022-02-26 16:36:27 -0600

    sd

--- showdown_lineup_builder.py
+++ showdown_lineup_builder.py
@@ -10,7 +10,10 @@ players = c.fetchall()
 file_text = "Position,Name + ID,Name,ID,Roster Position,Salary,Game Info,TeamAbbrev,AvgPointsPerGame,Max Deviation\n"
 for player in players:
     if player[11] != None:
-        file_text += f"{player[2]},{player[10]},{player[0]},{player[12]},{player[2]},{player[3]},{player[11]},{player[1]},{round(player[4],2)},{player[5]}\n"
+        if "CPT" in player:
+            file_text += f"{player[2]},{player[10]},{player[0]},{player[12]},{player[2]},{float(player[3] * 1.5)},{player[11]},{player[1]},{round(player[4],2)},{player[5]}\n"
+        else:
+            file_text += f"{player[2]},{player[10]},{player[0]},{player[12]},{player[2]},{player[3]},{player[11]},{player[1]},{round(player[4],2)},{player[5]}\n"
 
 
 with open("draft_from_file.csv","w") as f:
@@ -18,11 +21,14 @@ with open("draft_from_file.csv","w") as f:
 
 
 optimizer = get_optimizer(Site.DRAFTKINGS_CAPTAIN_MODE, Sport.BASKETBALL)
+optimizer.set_fantasy_points_strategy(RandomFantasyPointsStrategy())
 optimizer.load_players_from_csv("draft_from_file.csv")
+optimizer.set_min_salary_cap(48000)
+optimizer.settings.max_from_one_team = 4
 
 
 
-lineups = list(optimizer.optimize(28,max_exposure=1))
+lineups = list(optimizer.optimize(26,max_exposure=0.75))
 optimizer.export("result.csv")
 
 with open("result.csv",'r') as f:
@@ -30,7 +36,7 @@ with open("result.csv",'r') as f:
     text_string = "Name,Own%\n"
     for player in players:
         if player[0] in string:
-            actual_ownership = string.count(player[0])/ 28
+            actual_ownership = string.count(player[0])/ 26
             text_string += f"{player[0]},{actual_ownership}\n"
 
     with open("ownership.csv",'w') as q:
